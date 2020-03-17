7c8f35a2a1e018e8bb0b5420e981f23944f9a1af
SebastianKC <sebkc.sebco@gmail.com>
2020-03-17 19:41:54 +1100

    initial commit

---
+++ Assets/Scripts/CheckersBoard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckersBoard : MonoBehaviour
+{
+    public Piece[,] pieces = new Piece[8, 8];
+    public GameObject whitePiecePrefab;
+    public GameObject blackPiecePrefab;
+
+    private void GenerateBoard()
+    {
+        //Generate white team
+
+        //Generate black team
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# 3D Checkers
+
+3D Checkers Built in Unity with C# scripts.
+
+This was created by following a tutorial by [N3K EN](https://www.n3k.ca/) on YouTube.
