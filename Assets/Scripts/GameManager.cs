8a0cab3d877ddcc60017b2db13aac3e006fd9cd2
SebastianKC <sebkc.sebco@gmail.com>
2020-05-05 16:26:25 +1000

    Added lobby/meenu with main, connect, host variants. Created game manager to link all buttons to the script and their functions.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { set; get; }
+
+    public GameObject mainMenu;
+    public GameObject hostMenu;
+    public GameObject connectMenu;
+
+    private void Start()
+    {
+        Instance = this;
+        hostMenu.SetActive(false);
+        connectMenu.SetActive(false);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ConnectButton()
+    {
+        Debug.Log("Connect");
+        mainMenu.SetActive(false);
+        connectMenu.SetActive(true);
+    }
+    public void HostButton()
+    {
+        Debug.Log("Host");
+        mainMenu.SetActive(false);
+        hostMenu.SetActive(true);
+    }
+    public void ConnectToServerButton()
+    {
+        Debug.Log("Connect to server");
+    }
+    public void BackButton()
+    {
+        mainMenu.SetActive(true);
+        connectMenu.SetActive(false);
+        hostMenu.SetActive(false);
+    }
+}
