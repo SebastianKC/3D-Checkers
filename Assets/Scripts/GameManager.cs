58dad7849ce1ac1c053f5e2a76161f02b03d1295
SebastianKC <sebkc.sebco@gmail.com>
2020-05-05 21:18:16 +1000

    More host, server, client communication and connection tweaks

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +11,11 @@ public class GameManager : MonoBehaviour
     public GameObject mainMenu;
     public GameObject hostMenu;
     public GameObject connectMenu;
+
+    public GameObject serverPrefab;
+    public GameObject clientPrefab;
+
+    public InputField nameInput;
 
     private void Start()
     {
@@ -26,6 +33,22 @@ public class GameManager : MonoBehaviour
     }
     public void HostButton()
     {
+        try
+        {
+            Server s = Instantiate(serverPrefab).GetComponent<Server>();
+            s.Init();
+
+            Client c = Instantiate(clientPrefab).GetComponent<Client>();
+            c.clientName = nameInput.text;
+            if (c.clientName == "")
+                c.clientName = "Host";
+            c.ConnectToServer("127.0.0.1", 6321);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
         Debug.Log("Host");
         mainMenu.SetActive(false);
         hostMenu.SetActive(true);
@@ -33,11 +56,36 @@ public class GameManager : MonoBehaviour
     public void ConnectToServerButton()
     {
         Debug.Log("Connect to server");
+        string hostAddress = GameObject.Find("HostInput").GetComponent<InputField>().text;
+        if (hostAddress == "")
+            hostAddress = "127.0.0.1";
+
+        try
+        {
+            Client c = Instantiate(clientPrefab).GetComponent<Client>();
+            c.clientName = nameInput.text;
+            if (c.clientName == "")
+                c.clientName = "Client";
+            c.ConnectToServer(hostAddress, 6321);
+            connectMenu.SetActive(false);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
     }
     public void BackButton()
     {
         mainMenu.SetActive(true);
         connectMenu.SetActive(false);
         hostMenu.SetActive(false);
+
+        Server s = FindObjectOfType<Server>();
+        if (s != null)
+            Destroy(s.gameObject);
+                
+        Client c = FindObjectOfType<Client>();
+        if (c != null)
+            Destroy(s.gameObject);
     }
 }
